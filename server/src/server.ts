796b42d1490021e6656cc177943cc8d7589f5d4a
Romulo Mendes <romulo.gomes.mendes@hotmail.com>
2023-12-04 19:54:05 -0300

    use validation zod

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,39 +1,45 @@
-import { Prisma } from "prisma/client";
-
 import fastify from "fastify";
 import { prisma } from "./prisma";
+import { z } from "zod";
 
 const app = fastify();
 
 app.get("/", () => {
-	return "Test";
+  return "Test";
+});
+
+const createUserSchema = z.object({
+  email: z.string().email(),
+  nome: z.string(),
+  senha: z.string(),
+  apelido: z.string(),
+  fotoPerfil: z.string().optional(),
 });
 
 app.post("/usuario", async (req, res) => {
-	const { email, nome, senha, apelido, fotoPerfil } = req.body;
-
-	if (!email || !nome || !senha || !apelido) {
-		return res.status(400).send("Algum campo esta faltando");
-	}
-
-	try {
-		await prisma.user.create({
-			data: {
-				apelido,
-				email,
-				nome,
-				senha,
-				fotoPerfil,
-			},
-		});
-
-		return res.status(201).send({ message: "account created" });
-	} catch (error) {
-		console.log(error);
-		return res.status(500).send({ message: "internal error" });
-	}
+  try {
+    const ValidateData = createUserSchema.parse(req.body);
+    const { email, nome, senha, apelido, fotoPerfil } = ValidateData;
+    const user = await prisma.user.create({
+      data: {
+        apelido,
+        email,
+        nome,
+        senha,
+        fotoPerfil,
+      },
+    });
+    res.status(201).send({ message: "Usuário criado com sucesso" });
+  } catch (error) {
+    console.log(error);
+    if (error instanceof Error) {
+      res.status(400).send("Algum campo esta faltando");
+    } else {
+      res.status(500).send("Erro interno do servidor");
+    }
+  }
 });
 
 app.listen({ port: 3000 }).then(() => {
-	console.log("Server running");
+  console.log("Server running");
 });
