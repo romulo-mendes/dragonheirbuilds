c1be9ea84a9af640a089c6f8d80f7831bd2a1c46
Romulo Mendes <romulo.gomes.mendes@hotmail.com>
2023-12-05 19:36:44 -0300

    melhorias na tratativa de erros

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,15 +4,11 @@ import { z } from "zod";
 
 const app = fastify();
 
-app.get("/", () => {
-  return "Test";
-});
-
 const createUserSchema = z.object({
-  email: z.string().email({ message: "Insira um endereço de email valido" }),
-  nome: z.string(),
-  senha: z.string(),
-  apelido: z.string(),
+  email: z.string({required_error:"O campo Email é obrigatório"}).email({ message: "Insira um endereço de email valido" }),
+  nome: z.string({required_error:"O campo Nome é obrigatório"}),
+  senha: z.string({required_error:"O campo Senha é obrigatório"}),
+  apelido: z.string({required_error:"O campo Apelido é obrigatório"}),
   fotoPerfil: z.string().optional(),
 });
 
@@ -32,10 +28,12 @@ app.post("/usuario", async (req, res) => {
     res.status(201).send({ message: "Usuário criado com sucesso" });
   } catch (error) {
     console.log(error);
-    if (error instanceof Error) {
+    if (error instanceof z.ZodError) {
       res.status(400).send(error.message);
-    } else {
+    } else if (error instanceof Error) {
       res.status(500).send("Erro interno do servidor");
+    } else {
+      res.status(500).send("Erro desconhecido");
     }
   }
 });
