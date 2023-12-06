05ccf37251608df7ca862790372f15fed871370a
Romulo Mendes <romulo.gomes.mendes@hotmail.com>
2023-12-05 23:24:56 -0300

    criação de token e rota de login

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,66 @@
 import fastify from "fastify";
 import { prisma } from "./prisma";
 import { z } from "zod";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
 
 const app = fastify();
 
+const jwtSecret = "uw+c|2R*#=G'^k[}K3#zhkShl8&D#XiDSiX['FyxK;6=K8|iEEqV2f{'#Yq";
+
 const createUserSchema = z.object({
-  email: z.string({required_error:"O campo Email é obrigatório"}).email({ message: "Insira um endereço de email valido" }),
-  nome: z.string({required_error:"O campo Nome é obrigatório"}),
-  senha: z.string({required_error:"O campo Senha é obrigatório"}),
-  apelido: z.string({required_error:"O campo Apelido é obrigatório"}),
+  email: z
+    .string({ required_error: "O campo Email é obrigatório" })
+    .email({ message: "Insira um endereço de email valido" }),
+  nome: z.string({ required_error: "O campo Nome é obrigatório" }),
+  senha: z.string({ required_error: "O campo Senha é obrigatório" }),
+  apelido: z.string({ required_error: "O campo Apelido é obrigatório" }),
   fotoPerfil: z.string().optional(),
+});
+const loginSchema = z.object({
+  email: z
+    .string({ required_error: "O campo Email é obrigatório" })
+    .email({ message: "Insira um endereço de email valido" }),
+  senha: z.string({ required_error: "O campo Senha é obrigatório" }),
+});
+
+function generateToken(userId: number, email: string): string {
+  return sign({ userId, email }, jwtSecret, { expiresIn: "1h" });
+}
+
+app.post("/login", async (req, res) => {
+  try {
+    const validatedData = loginSchema.parse(req.body);
+    const { email, senha } = validatedData;
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      res.status(401).send("Credenciais inválidas");
+      return;
+    }
+
+    if (senha !== user.senha) {
+      res.status(401).send("Credenciais inválidas");
+      return;
+    }
+
+    const token = generateToken(user.id, user.email);
+
+    res.status(200).send({
+      message: "Login bem-sucedido",
+      user: { id: user.id, email, nome: user.nome },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    if (error instanceof z.ZodError) {
+      res.status(400).send(error.message);
+    } else if (error instanceof Error) {
+      res.status(500).send("Erro interno do servidor");
+    } else {
+      res.status(500).send("Erro desconhecido");
+    }
+  }
 });
 
 app.post("/usuario", async (req, res) => {
