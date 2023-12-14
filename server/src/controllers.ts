3451d9a1cd751c5c1177a26b1a58db9d6e9d4af1
nanerseasac <renan.casaeses@gmail.com>
2023-12-14 16:39:10 -0300

    controllers route

---
+++ server/src/controllers.ts
@@ -0,0 +1,87 @@
+import { prisma } from "./prisma";
+import { z } from "zod";
+import { sign } from "jsonwebtoken";
+import { FastifyReply, FastifyRequest } from "fastify";
+
+const jwtSecret = "uw+c|2R*#=G'^k[}K3#zhkShl8&D#XiDSiX['FyxK;6=K8|iEEqV2f{'#Yq";
+
+const createUserSchema = z.object({
+	email: z
+		.string({ required_error: "O campo Email é obrigatório" })
+		.email({ message: "Insira um endereço de email valido" }),
+	nome: z.string({ required_error: "O campo Nome é obrigatório" }),
+	senha: z.string({ required_error: "O campo Senha é obrigatório" }),
+	apelido: z.string({ required_error: "O campo Apelido é obrigatório" }),
+	fotoPerfil: z.string().optional(),
+});
+const loginSchema = z.object({
+	email: z
+		.string({ required_error: "O campo Email é obrigatório" })
+		.email({ message: "Insira um endereço de email valido" }),
+	senha: z.string({ required_error: "O campo Senha é obrigatório" }),
+});
+
+function generateToken(userId: number, email: string): string {
+	return sign({ userId, email }, jwtSecret, { expiresIn: "1h" });
+}
+
+export const login = async (req: FastifyRequest, res: FastifyReply) => {
+	try {
+		const validatedData = loginSchema.parse(req.body);
+		const { email, senha } = validatedData;
+		const user = await prisma.user.findUnique({ where: { email } });
+
+		if (!user) {
+			res.status(401).send("Credenciais inválidas");
+			return;
+		}
+
+		if (senha !== user.senha) {
+			res.status(401).send("Credenciais inválidas");
+			return;
+		}
+
+		const token = generateToken(user.id, user.email);
+
+		res.status(200).send({
+			message: "Login bem-sucedido",
+			user: { id: user.id, email, nome: user.nome },
+			token,
+		});
+	} catch (error) {
+		console.log(error);
+		if (error instanceof z.ZodError) {
+			res.status(400).send(error.message);
+		} else if (error instanceof Error) {
+			res.status(500).send("Erro interno do servidor");
+		} else {
+			res.status(500).send("Erro desconhecido");
+		}
+	}
+};
+
+export const addUser = async (req: FastifyRequest, res: FastifyReply) => {
+	try {
+		const ValidateData = createUserSchema.parse(req.body);
+		const { email, nome, senha, apelido, fotoPerfil } = ValidateData;
+		const user = await prisma.user.create({
+			data: {
+				apelido,
+				email,
+				nome,
+				senha,
+				fotoPerfil,
+			},
+		});
+		res.status(201).send(user);
+	} catch (error) {
+		console.log(error);
+		if (error instanceof z.ZodError) {
+			res.status(400).send(error.message);
+		} else if (error instanceof Error) {
+			res.status(500).send("Erro interno do servidor");
+		} else {
+			res.status(500).send("Erro desconhecido");
+		}
+	}
+};
